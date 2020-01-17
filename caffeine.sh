3d4f0c202b075e93190fa6dd911dbf09d6636049
Oliver Barnwell <barnwelloliver@gmail.com>
2020-01-17 10:20:00 +0000

    initial

---
+++ caffeine.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+access_token=$1
+account_id=$2
+
+
+caffeine_sources="\"nero\",\"cafe\",\"costa\",\"pret\",\"starbucks\",\"amt\",\"coffee\""
+
+first_of_month=$(date +%Y-%m-%dT00:00:00Z -d "`date +%Y%m01`")
+
+api_root="https://api.monzo.com"
+auth="Authorization: Bearer $access_token"
+
+transactions=$(curl -s -H "$auth" "$api_root/transactions?account_id=$account_id&since=$first_of_month")
+
+amounts=$(echo $transactions | jq '.[] | .[] | {desc: .description, amount: .amount} | select(.desc | ascii_downcase | contains('"$caffeine_sources"')) | .amount')
+
+sum_transactions=$(echo $amounts | sed 's/ /+/g' | bc | sed 's/-//g')
+scaled_sum=$(echo "scale = 2; $sum_transactions / 100" | bc)
+
+echo "Awake? You should be - you've spent £$scaled_sum on caffeine since the start of the month ☕😅."
+
